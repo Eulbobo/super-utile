2fc11aceebe9e204281555bedb383af406804209
JulienBO <J.BONIOL-prestataire@alptis.fr>
2016-03-03 12:00:33 +0100

    Commit initial d'une mauvaise idée

---
+++ src/main/java/superbad/idea/builder/MetaBuilder.java
@@ -0,0 +1,67 @@
+package superbad.idea.builder;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class MetaBuilder<T> {
+
+    private T beanToBuild;
+
+    private Map<String, Object> mapSet = new HashMap<>();
+
+    public static <T> MetaBuilder<T> getBuilder(Class<T> classBean) {
+        return new MetaBuilder<>(classBean);
+    }
+
+    public static <T> MetaBuilder<T> getBuilder(T bean) {
+        return new MetaBuilder<>(bean);
+    }
+
+    private static void makeAccessible(AccessibleObject obj) {
+        if (!obj.isAccessible()) {
+            obj.setAccessible(true);
+        }
+    }
+
+    private MetaBuilder(Class<T> classBean) {
+        try {
+            Constructor<T> constructor = classBean.getDeclaredConstructor();
+            makeAccessible(constructor);
+            this.beanToBuild = constructor.newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalArgumentException("Cannot instantiate this object", e);
+        }
+    }
+
+    private MetaBuilder(T bean) {
+        this.beanToBuild = bean;
+    }
+
+    public MetaBuilder<T> setProperty(String propertyName, Object property) {
+        mapSet.put(propertyName, property);
+        return this;
+    }
+
+    private void setFieldProperty(String propertyName, Object property) {
+        try {
+            Field field = beanToBuild.getClass()
+                    .getDeclaredField(propertyName);
+            makeAccessible(field);
+            field.set(beanToBuild, property);
+        } catch (Exception e) {
+            // nothing to catch here
+        }
+    }
+
+    public T build() {
+        for (Entry<String, Object> entry : mapSet.entrySet()) {
+            setFieldProperty(entry.getKey(), entry.getValue());
+        }
+        return beanToBuild;
+    }
+
+}
---
+++ src/test/java/superbad/idea/builder/MetaBuilderTest.java
@@ -0,0 +1,39 @@
+package superbad.idea.builder;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static superbad.idea.builder.MetaBuilder.getBuilder;
+
+public class MetaBuilderTest {
+
+    public static class MyLocalClass {
+        private String value;
+        public MyLocalClass(){
+            // public constructor
+        }
+        public String getValue(){ return value; }
+    }
+
+    @Test
+    public void should_set_property_on_private_field(){
+
+       MyLocalClass loc = getBuilder(new MyLocalClass()).setProperty("value", "MesCouilles").build();
+
+       assertThat(loc).isNotNull();
+       assertThat(loc.getValue()).isNotNull().isEqualTo("MesCouilles");
+    }
+
+    @Test
+    public void should_set_last_property_on_private_field() throws ReflectiveOperationException{
+
+       MyLocalClass loc = getBuilder(MyLocalClass.class)
+                   .setProperty("value", "MesCouilles")
+                   .setProperty("value", "Mickey")
+                   .setProperty("asterix", "vide")
+                   .build();
+
+       assertThat(loc).isNotNull();
+       assertThat(loc.getValue()).isNotNull().isEqualTo("Mickey");
+    }
+}
